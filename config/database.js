06c274bbaaac8024fb02926fb2179ac92ee75d90
wujzan <xmine777@gmail.com>
2023-07-16 12:44:10 +0800

    modify

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //database:'mongodb://localhost:27017/farmer'
-    database:'mongodb+srv://farmer_zones:K5JhHNbAGWqVhwj7@farmer.gc0ko.mongodb.net/farmer?retryWrites=true&w=majority'
+    //database:'mongodb://localhost:27017/farmer'    
 }
